bcf2d1c3ce13d86552746a2402a0a60cdf638422
Greg Woods <gregwoodslancs@gmail.com>
2024-01-12 22:41:22 +0000

    better utilise asyncio to simplify the code

--- lapcounter-server-lapdata/create_lap_data.py
+++ lapcounter-server-lapdata/create_lap_data.py
@@ -1,11 +1,3 @@
-# subscribe to mqtt topic and print message to console for testing
-
-# About asyncio...
-#   why do i need asyncio in this code?
-#     it only does one thing, generate driver data at random intervals.
-#   The real script may benefit... 
-#     if an mqtt pub was blocking, it may hold up the reading of another driver's lap
-
 import asyncio
 import json
 import random
@@ -17,13 +9,13 @@ import paho.mqtt as mqtt
 
 PORT = 8080
 
-numberOfDrivers = 5
+numberOfDrivers = 6
 baseLapTime = 5
 abilityRangeMax = 4.2
 lapTimeRangeMax = 3.5
 outlierExtraLapTimeMin = 5
 outlierExtraLapTime = 13
-outlierLowestFrequency = 5 #best driver will average a bad lap every 30 laps
+outlierLowestFrequency = 5
 
 
 class Driver:
@@ -50,43 +42,30 @@ class Driver:
         print("Next Lap At: ", self.nextLapAt)
 
 
-async def send_message():
-    print("send_message")
-    #Create a list of drivers each with their own abilities
-    driverRange = [i for i in range(1,numberOfDrivers+1)]
-    drivers = list(map(lambda n: Driver(n, time.time()), driverRange))
 
+
+async def send_message(driver):
     async with aiomqtt.Client("localhost") as client:
         while True:
-            #Since I'm unure how to have 6 separate timers going, each with their own schedule, 
-            #  I'm going to fake it by keeping a running total of system time for each driver
-            #  and comparing to current system time
-
-            timenow = time.time()
-
-            for driver in drivers:
-                
-                #if timenow > driver.nextLapAt and driver.driverNumber != 2:    #driver 2 doesn't post any laps
-                if timenow > driver.nextLapAt:
-                    driver.dbg()    
-                    lapdata = {"type": "lap", "car": driver.driverNumber, "time": driver.nextLapAt}
-                    lapjson = json.dumps(lapdata)
-                    await client.publish("lap", payload=lapjson)
-                    print(lapdata)
-                    driver.generateLap()
-
-            await asyncio.sleep(0.001)
+            driver.generateLap()
+            await asyncio.sleep(max(0, driver.nextLapAt - time.time()))            
+            driver.dbg()    
+            lapdata = {"type": "lap", "car": driver.driverNumber, "time": driver.nextLapAt}
+            lapjson = json.dumps(lapdata)
+            await client.publish("lap", payload=lapjson)
+            print(lapdata)
 
 
+# Create a list of drivers each with their own abilities
+driverRange = [i for i in range(1,numberOfDrivers+1)]
+drivers = list(map(lambda n: Driver(n, time.time()), driverRange))
+
+
+# Create a task for each driver
 async def schedule_tasks():
-    # running tasks concurrently
-    # We could have each driver have their own task, each task waits its own
-    #   calculated random period before firing.
     async with asyncio.TaskGroup() as tg:
-        task1 = tg.create_task(
-            send_message())
-        #task2 = tg.create_task(
-        #    everyFiveSeconds())
+        for driver in drivers:
+            tg.create_task(send_message(driver))       
 
 
 # Change to the "Selector" event loop if platform is Windows
