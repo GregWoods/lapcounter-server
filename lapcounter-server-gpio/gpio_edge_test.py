5d7a87165cdc720ce9fbdd02ff814447197f539a
GregWoods <gregwoodslancs@gmail.com>
2024-04-13 16:30:06 +0100

    simplest edge detect sample. Run as sudo

--- lapcounter-server-gpio/gpio_edge_test.py
+++ lapcounter-server-gpio/gpio_edge_test.py
@@ -1,10 +1,14 @@
+# Must run inmder sudo
+#   or we get error: RuntimeError: Failed to add edge detection
+
 import RPi.GPIO as GPIO
 import asyncio
 
-pwr_btn_pin = 3
+pwr_btn_gpio = 3
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pwr_btn_pin, GPIO.IN)
-GPIO.add_event_detect(pwr_btn_pin, GPIO.RISING, callback=lambda _ : print("button pressed!"))
+GPIO.setup(pwr_btn_gpio, GPIO.IN)
+
+GPIO.add_event_detect(pwr_btn_gpio, GPIO.RISING, callback=lambda _ : print("button pressed!"))
 
 async def main():
     while True:
