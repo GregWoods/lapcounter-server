06b59f650cca32821dc74bce2aa772cb6e07ac8d
GregWoods <gregwoodslancs@gmail.com>
2024-04-13 09:01:19 +0100

    env updates from the pi

--- lapcounter-server-gpio/setenv.sh
+++ lapcounter-server-gpio/setenv.sh
@@ -1,5 +1,7 @@
 # run using 'source' command (alias '.')
 #    . ./setenv.sh
+# ordinarily these environment variables are not available to commands run as sudo.
+#   so we must run our script with the "-E" parameter, to preserve these variables
 
 export MQTT_HOSTNAME=127.0.0.1
 export MINIMUM_LAP_TIME=3.0
