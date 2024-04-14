b98c089cb42d280e255efbf06074eafad915758e
Greg Woods <gregwoodslancs@gmail.com>
2024-04-14 09:14:56 +0100

    added new lane number env variable for testing

--- lapcounter-server-gpio/setenv.sh
+++ lapcounter-server-gpio/setenv.sh
@@ -3,6 +3,7 @@
 # ordinarily these environment variables are not available to commands run as sudo.
 #   so we must run our script with the "-E" parameter, to preserve these variables
 
+export LANE_NUMBER=1
 export MQTT_HOSTNAME=127.0.0.1
 export MINIMUM_LAP_TIME=3.0
 
