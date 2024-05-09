9e1382b0047b081b8f8fb998dbc495434110c1dc
Greg Woods <gregwoodslancs@gmail.com>
2024-05-09 23:29:39 +0100

    remove excess output and comments

--- lapcounter-server-lapdata/timestamps_to_lapdata.py
+++ lapcounter-server-lapdata/timestamps_to_lapdata.py
@@ -11,6 +11,9 @@ print(f"MQTT_HOSTNAME: {mqtt_hostname}")
 min_lap_time = int(os.getenv('MINIMUM_LAP_TIME')) * 1e9
 print(f"MINIMUM_LAP_TIME: {min_lap_time}")
 
+publish_topic = "lap"
+print(f"Publishing to topic: {publish_topic}")
+
 
 #get a datetime from a time_ns() value
 def get_datetime_from_ns(ns):
@@ -22,22 +25,19 @@ prevtimelist = [time.time_ns() - min_lap_time - 1 for _ in range(6)]
 
 
 def send_lap_data(lap_data_json):
-    print(f"lap_data_json: {lap_data_json}")
-    client.publish("lap", payload=lap_data_json)
+    print(f"{publish_topic}: {lap_data_json}")
+    client.publish(publish_topic, payload=lap_data_json)
 
 
 def message_received(_client, _userdata, msg):
     topic = msg.topic
     m_decode = str(msg.payload.decode("utf-8","ignore"))
     data = json.loads(m_decode)
-    print(f"Incoming timestamp data: {data}")
     carnumber = data['car']
     caridx = carnumber - 1
     #calculate laptime for this car
     thistime = time.time_ns()
-    print(f"thistime: {thistime}")
     prevtime = prevtimelist[caridx]
-    print(f"prevtime: {prevtime}")
     lapsedtime = thistime - prevtime
     #if this is a realistic time then send it
     if lapsedtime > min_lap_time:
