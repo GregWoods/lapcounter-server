4993328fc69a9e8dad067549ac3f3f2b3c13fd12
Greg Woods <gregwoodslancs@gmail.com>
2024-07-05 23:01:16 +0100

    changes to raspi-setup.sh

--- raspi-setup.sh
+++ raspi-setup.sh
@@ -1,10 +1,11 @@
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Get all required files
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-rm get-docker.sh
 curl -O https://raw.githubusercontent.com/GregWoods/lapcounter-server/main/compose.prod.yaml
 curl -O https://raw.githubusercontent.com/GregWoods/lapcounter-server/main/.env.prod
 mkdir -p ./mosquitto/config
 curl -o ./mosquitto/config/mosquitto.conf  https://raw.githubusercontent.com/GregWoods/lapcounter-server/main/mosquitto/config/mosquitto.conf
 
-#sudo docker compose --file compose.prod.yaml up --detach
+# Run the rest manually, because I can't figure out how to do it in a script!
+# for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# sudo sh get-docker.sh && rm get-docker.sh
+# sudo docker compose --file compose.prod.yaml up --detach
