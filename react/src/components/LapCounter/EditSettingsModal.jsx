ec336152f9425502c2a3f78a2ae8dcb1b90de612
Greg Woods <gregwoodslancs@gmail.com>
2024-08-03 01:27:38 +0100

    renames only

--- react/src/components/LapCounter/EditSettingsModal.jsx
+++ react/src/components/LapCounter/EditSettingsModal.jsx
@@ -3,20 +3,19 @@ import {useState} from 'react';
 import ReactModal from 'react-modal';
 
 
-
-const EditSettingsModal = ({ showMe, onClose, wsUrl, setWsUrl }) => 
+const EditSettingsModal = ({ showMe, onClose, mqttHost, setMqttHost }) => 
 {
     //copy parent state to local state so we can modify it independently 
     //  and save it only when the dialog is "Done""
-    const [_wsUrl, _setWsUrl] = useState(wsUrl);
+    const [localMqttHost, setLocalMqttHost] = useState(mqttHost);
 
     const onDone = () => {
-        setWsUrl(_wsUrl);
+        setMqttHost(localMqttHost);
         onClose();
     }
     const onCancel = () => {
         //reset local _wsUrl, or it will be remembered next time the modal is opened. Feels clunky
-        _setWsUrl(wsUrl);
+        setLocalMqttHost(mqttHost);
         onClose();
     }
  
@@ -41,8 +40,8 @@ const EditSettingsModal = ({ showMe, onClose, wsUrl, setWsUrl }) =>
                 <div>
                     <label htmlFor="config_wsaddress">WebSocket URL</label>
                     <input id="config_wsaddress" name="wsurl" autoFocus
-                    onChange={(evt) => {_setWsUrl(evt.target.value)}}
-                    value={_wsUrl} />
+                    onChange={(evt) => {setLocalMqttHost(evt.target.value)}}
+                    value={localMqttHost} />
                 </div>
 
                 <h2><button onClick={onDone}>Done</button></h2>
