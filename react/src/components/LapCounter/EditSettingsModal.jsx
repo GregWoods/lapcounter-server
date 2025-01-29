fe67773e3c25109c96b05d6c77ae969d082322ca
Greg Woods <gregwoodslancs@gmail.com>
2025-01-29 00:31:26 +0000

    SettingsModal is currently unused, remove mqtt settings which are now in env vars

--- react/src/components/LapCounter/EditSettingsModal.jsx
+++ react/src/components/LapCounter/EditSettingsModal.jsx
@@ -1,21 +1,21 @@
 import './EditSettingsModal.css';
-import {useState} from 'react';
+//import {useState} from 'react';
 import ReactModal from 'react-modal';
 
 
-const EditSettingsModal = ({ showMe, onClose, mqttHost, setMqttHost }) => 
+const EditSettingsModal = ({ showMe, onClose, }) => 
 {
     //copy parent state to local state so we can modify it independently 
     //  and save it only when the dialog is "Done""
-    const [localMqttHost, setLocalMqttHost] = useState(mqttHost);
+    //const [localMqttHost, setLocalMqttHost] = useState(mqttHost);
 
     const onDone = () => {
-        setMqttHost(localMqttHost);
+        //setMqttHost(localMqttHost);
         onClose();
     }
     const onCancel = () => {
         //reset local _wsUrl, or it will be remembered next time the modal is opened. Feels clunky
-        setLocalMqttHost(mqttHost);
+        //setLocalMqttHost(mqttHost);
         onClose();
     }
  
@@ -33,15 +33,14 @@ const EditSettingsModal = ({ showMe, onClose, mqttHost, setMqttHost }) =>
             <form onSubmit={evt => evt.preventDefault() }>
                 <h2>Edit Settings</h2>
         
-                <p>For Raspi3A version, running Chromium on the PI with a connected monitor, this value is 
-                <b> ws://127.0.0.1:8080</b></p>
-                <p>If you are running this on a remote web browser, you need to find the IP address of the 
-                    Raspberry Pi and use that value in place of 127.0.0.1</p>
+                <p>Description</p>
+/
                 <div>
-                    <label htmlFor="config_wsaddress">WebSocket URL</label>
-                    <input id="config_wsaddress" name="wsurl" autoFocus
-                    onChange={(evt) => {setLocalMqttHost(evt.target.value)}}
-                    value={localMqttHost} />
+                    <label htmlFor="blahInput">WebSocket URL</label>
+                    <input id="blahInput" name="myInput" autoFocus
+                    onChange={() => {
+                        //setLocalMqttHost(evt.target.value)
+                    }} />
                 </div>
 
                 <h2><button onClick={onDone}>Done</button></h2>
